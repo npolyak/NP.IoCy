651229418330c552ea0659ae2f785ac5d3a7c9b2
npolyak <nick.polyak@verizon.net>
2018-03-19 21:18:19 -0400

    added NP.IoC repository

---
+++ IoCContainer.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyIoCContainerTest
+{
+    class TypeMapCell
+    {
+        public Type BaseType { get; set; }
+
+        public List<Type> TargetTypes { get; } = new List<Type>();
+
+        public Type GetTargetType() =>
+            TargetTypes.LastOrDefault();
+    }
+
+    public class IoCContainer
+    {
+        Dictionary<Type, TypeMapCell> _typeMap = new Dictionary<Type, TypeMapCell>();
+
+        public void Register(Type baseType, Type targetType)
+        {
+            if (!targetType.SelfOrSuperTypeMatches(baseType))
+            {
+                throw new Exception($"Cannot assign type {targetType.Name} to {baseType.Name}");
+            }
+
+            if (!_typeMap.TryGetValue(baseType, out TypeMapCell typeMapCell))
+            {
+                typeMapCell = new TypeMapCell();
+            }
+
+            typeMapCell.TargetTypes.Add(targetType);
+            _typeMap[baseType] = typeMapCell;
+        }
+
+        public void Unregister(Type baseType, Type targetType)
+        {
+            if (_typeMap.TryGetValue(baseType, out TypeMapCell typeMapCell))
+            {
+                typeMapCell.TargetTypes.Remove(targetType);
+
+                if (typeMapCell.TargetTypes.Count == 0)
+                {
+                    _typeMap.Remove(baseType);
+                }
+            }
+        }
+
+        public void Unregister(Type baseType)
+        {
+            _typeMap.Remove(baseType);
+        }
+
+        public void Register<TBase, TTarget>()
+            where TTarget : TBase
+        {
+            Register(typeof(TBase), typeof(TTarget));
+        }
+
+        public void Unregister<TBase, TTarget>()
+        {
+            Unregister(typeof(TBase), typeof(TTarget));
+        }
+
+        public void Unregister<TBase>() =>
+            Unregister(typeof(TBase));
+
+        public TResult GetTargetTypeImpl<TBase, TResult>(Func<IEnumerable<Type>, TResult> resultGetter)
+        {
+            Type baseType = typeof(TBase);
+
+            if (baseType.IsConstructedGenericType)
+            {
+                baseType = baseType.GetGenericTypeDefinition();
+            }
+
+            if (_typeMap.TryGetValue(baseType, out TypeMapCell cell))
+            {
+                return resultGetter(cell.TargetTypes);
+            }
+
+            return default(TResult);
+        }
+
+
+        public Type GetTargetType<TBase>() =>
+            GetTargetTypeImpl<TBase, Type>(targetTypes => targetTypes.Last());
+
+        public IEnumerable<Type> GetAllTargetTypes<TBase>() =>
+            GetTargetTypeImpl<TBase, IEnumerable<Type>>(targetTypes => targetTypes);
+
+        public TBase GetTargetObj<TBase>(bool force = false, params object[] args)
+        {
+            Type targetType = GetTargetType<TBase>();
+
+            if (targetType == null)
+            {
+                if (force)
+                {
+                    targetType = typeof(TBase);
+                }
+                else
+                {
+                    return default(TBase);
+                }
+            }
+            else
+            {
+
+            }
+
+            return (TBase) Activator.CreateInstance(targetType, args);
+        }
+
+        public IEnumerable<TBase> GetAllTargetObjs<TBase>()
+        {
+            IEnumerable<Type> allTargetTypes = GetAllTargetTypes<TBase>();
+
+            IList<TBase> results = new List<TBase>();
+
+            if (allTargetTypes == null)
+                return results;
+
+            foreach(Type type in allTargetTypes)
+            {
+                TBase obj = (TBase)Activator.CreateInstance(type);
+
+                results.Add(obj);
+            }
+
+            return results;
+        }
+    }
+}
---
+++ ReflectionUtils.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyIoCContainerTest
+{
+    public static class ReflectionUtils
+    {
+        public static string GetFullTypeStr(this Type type)
+        {
+            return type.Namespace + "." + type.Name;
+        }
+
+        public static bool TypesMatch(this Type sourceType, Type targetType)
+        {
+            return sourceType.GetFullTypeStr() == targetType.GetFullTypeStr();
+        }
+
+        public static IEnumerable<Type> GetSelfAndSuperTypes(this Type type)
+        {
+            if (type == null)
+                yield break;
+
+            yield return type;
+
+            while (true)
+            {
+                type = type.BaseType;
+
+                if (type == null)
+                    yield break;
+
+                yield return type;
+            }
+        }
+
+        public static IEnumerable<Type> GetSelfSuperTypesAndInterfaces(this Type type)
+        {
+            List<Type> result = new List<Type>();
+
+            if (type == null)
+                return result;
+
+            result.Add(type);
+
+            result.AddRange(type.BaseType.GetSelfSuperTypesAndInterfaces());
+
+            foreach (Type iface in type.GetInterfaces())
+            {
+                result.AddRange(iface.GetSelfSuperTypesAndInterfaces());
+            }
+
+            return result;
+        }
+
+        public static bool SelfOrSuperTypeMatches(this Type type, Type typeToMatch)
+        {
+            return type.GetSelfSuperTypesAndInterfaces().FirstOrDefault((t) => t.TypesMatch(typeToMatch)) != null;
+        }
+    }
+
+}
