7a9f9acc7867933a14c1170dd58bafa598475eb8
npolyak <nick.polyak@verizon.net>
2018-05-13 22:32:11 -0400

    added copyright notice.

--- TESTS/PluginsTest/Program.cs
+++ TESTS/PluginsTest/Program.cs
@@ -1,5 +1,16 @@
 ﻿using Interfaces;
 using NP.IoCy;
+// (c) Nick Polyak 2018 - http://awebpros.com/
+// License: Apache License 2.0 (http://www.apache.org/licenses/LICENSE-2.0.html)
+//
+// short overview of copyright rules:
+// 1. you can use this framework in any commercial or non-commercial 
+//    product as long as you retain this copyright message
+// 2. Do not blame the author of this software if something goes wrong. 
+// 
+// Also, please, mention this software in any documentation for the 
+// products that use it.
+
 using System.Collections.Generic;
 
 namespace PluginsTest
