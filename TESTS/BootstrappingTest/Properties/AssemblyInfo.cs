9f7b531cee2c2242fd49a1ca8ef341d9bf2cead2
npolyak <nick.polyak@verizon.net>
2018-05-16 18:15:16 -0400

    checkpoint

--- TESTS/BootstrappingTest/Properties/AssemblyInfo.cs
+++ TESTS/BootstrappingTest/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OrganizationTest")]
+[assembly: AssemblyTitle("BootstrappingTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("OrganizationTest")]
+[assembly: AssemblyProduct("BootstrappingTest")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
