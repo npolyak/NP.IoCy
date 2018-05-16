08527992079f37024bd44fdb9584c4ed2ab9679d
npolyak <nick.polyak@verizon.net>
2018-05-16 18:19:08 -0400

    fixed assembly names

--- TESTS/DynamicAssemblyLoadingTest/Properties/AssemblyInfo.cs
+++ TESTS/DynamicAssemblyLoadingTest/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OrganizationTest")]
+[assembly: AssemblyTitle("DynamicAssemblyLoadingTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("OrganizationTest")]
+[assembly: AssemblyProduct("DynamicAssemblyLoadingTest")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- TESTS/MultyPartBootstrappingTest/Properties/AssemblyInfo.cs
+++ TESTS/MultyPartBootstrappingTest/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OrganizationTest")]
+[assembly: AssemblyTitle("MultyPartBootstrappingTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("OrganizationTest")]
+[assembly: AssemblyProduct("MultyPartBootstrappingTest")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
