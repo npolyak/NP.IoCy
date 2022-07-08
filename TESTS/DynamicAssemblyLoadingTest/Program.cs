94788e773fc917535abccf1cd5814ad83ff0e17f
npolyak <nick.polyak@verizon.net>
2022-07-07 23:52:11 -0400

    fixed and added MIT license to code files

--- TESTS/DynamicAssemblyLoadingTest/Program.cs
+++ TESTS/DynamicAssemblyLoadingTest/Program.cs
@@ -13,6 +13,17 @@ using NP.IoCy;
 using NP.Utilities;
 using DynamicAssemblyLoadingTest.Interfaces;
 using System;
+// (c) Nick Polyak 2022 - http://awebpros.com/
+// License: MIT License (https://opensource.org/licenses/MIT)
+//
+// short overview of copyright rules:
+// 1. you can use this framework in any commercial or non-commercial 
+//    product as long as you retain this copyright message
+// 2. Do not blame the author of this software if something goes wrong. 
+// 
+// Also, please, mention this software in any documentation for the 
+// products that use it.
+
 using System.IO;
 
 using ILog = DynamicAssemblyLoadingTest.Interfaces.ILog;
